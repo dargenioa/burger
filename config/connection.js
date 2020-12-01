f9b5a54fa5bdde4430d875e31d26902d73a4c927
Amanda D'Argenio <dargenioa14@gmail.com>
2020-11-30 19:36:07 -0500

    created mysql schema and seed files, established connection in connection.js

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql");
+const inquirer = require("express");
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`connected as id ${connection.threadId}`);
+});
