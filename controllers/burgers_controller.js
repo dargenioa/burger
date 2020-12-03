ff9749f38c318ae624695a58565fe53a850ac503
Amanda D'Argenio <dargenioa14@gmail.com>
2020-12-02 19:08:09 -0500

    controllers.js CRUD created and working GET/POST/PUT/DELETE requests

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,58 +1,55 @@
 const express = require("express");
 const router = express.Router();
+const burger = require("../models/burger");
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
-// // Create all our routes and set up logic within those routes where required.
-// router.get("/quotes", function(req, res) {
-//     quote.all(function(data) {
-//     res.json({ quotes: data });
-//   });
-// });
+router.get("/burgers", function (req, res) {
+  burger.all(function (data) {
+    res.json({ burgers: data });
+  })
+});
 
-// router.post("/quotes", function(req, res) {
-//   quote.create([
-//     "author", "quote"
-//   ], [
-//     req.body.author, req.body.quote
-//   ], function(result) {
-//     // Send back the ID of the new quote
-//     res.json({ id: result.insertId });
-//   });
-// });
+router.post("/burgers", function (req, res) {
+  burger.create([
+    "burger_name", "devoured"
+  ], [
+    req.body.burger_name, req.body.devoured
+  ], function (result) {
+    res.json({ id: result.insertId });
+  });
+});
 
-// router.put("/quotes/:id", function(req, res) {
-//   const condition = "id = " + req.params.id;
+router.put("/burgers/:id", function(req, res){
+  let condition = `id = ${req.params.id}`;
 
-//   console.log("condition", condition);
+  console.log(condition);
 
-//   quote.update({
-//     author: req.body.author,
-//     quote: req.body.quote
-//   }, condition, function(result) {
-//     if (result.changedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.json({ id: req.params.id });
-//     }
-//   });
-// });
+  burger.update({
+    burger_name: req.body.burger_name,
+    devoured: req.body.devoured
+   }, condition, function (result) {
+     if (result.changedRows == 0) {
+       return res.status(404).end();
+     } else {
+       res.json({ id: req.params.id });
+     }
+   });
+});
 
-// router.delete("/quotes/:id", function(req, res) {
-//   const condition = "id = " + req.params.id;
+router.delete("/burgers/:id", function (req, res) {
+  let condition = `id = ${req.params.id}`;
 
-//   quote.delete(condition, function(result) {
-//     if (result.affectedRows == 0) {
-//       // If no rows were changed, then the ID must not exist, so 404
-//       return res.status(404).end();
-//     } else {
-//       res.status(200).end();
-//     }
-//   });
-// });
+  burger.delete(condition, function(result){
+    if (result.affectedRow == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 
 // Export routes for server.js to use.
 module.exports = router;
