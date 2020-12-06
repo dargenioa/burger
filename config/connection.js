5ca95dead7d1549ff5774e2e29ac3ad9f3e3e557
Amanda D'Argenio <dargenioa14@gmail.com>
2020-12-05 20:40:06 -0500

    created JAWS database for heroku

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,6 @@ const mysql = require("mysql");
 
 let connection;
 
-// if (process.env.JAWSDB_URL) {
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-//     connection = mysql.createConnection({
-//         host: "localhost",
-//         port: 3306,
-//         user: process.env.LOCAL_DB_USER,
-//         password: process.env.LOCAL_DB_PASSWORD,
-//         database: process.env.LOCAL_DB_DATABASE
-//     });
-// };
-
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
--- db/schema.sql
+++ db/schema.sql
@@ -1,8 +1,8 @@
-USE burgers_db;
 
 CREATE TABLE burgers(
 id INT NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(40),
 devoured BOOLEAN DEFAULT false,
+createdAt TIMESTAMP NOT NULL,
 PRIMARY KEY (id)
 );
